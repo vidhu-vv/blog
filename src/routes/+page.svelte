3623f1fbc77f51b048b39c89b82c3187f59580fa
VidhuV <79124406+vidhu-vv@users.noreply.github.com>
2023-12-23 15:50:34 -0800

    started home

---
+++ src/routes/+page.svelte
@@ -0,0 +1,23 @@
+<script>
+    import { goto } from '$app/navigation';
+</script>
+<style>
+    div {
+        font-family: 'Major Mono Display';font-size: 22px;
+    }
+</style>
+    
+<div class="flex flex-col overflow-hidden h-screen">
+    <div class="h-screen flex flex-col col-span-3">
+        <div class="flex-1 w-full">
+            <div class="animate-text my-16 bg-white border-4 border-secondary w-[90%] h-80 mx-auto grid place-items-center rounded-3xl">
+                <div class="text-justify w-[26.5rem] text-secondary">
+                    <h1 class="text-7xl font-bold uppercase">
+                        Vidhu
+                    </h1> 
+                </div>
+            </div>
+        </div>
+
+    </div>
+</div>
---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{html,js,svelte,ts}'],
+  theme: {
+    extend: {
+      colors: {
+        "primary": "var(--primary)",
+        "secondary": "var(--secondary)",
+        "background": "var(--background)",
+        "text": "var(--text)",
+        "accent": "var(--accent)",
+        // #004ba8
+      },
+    },
+  },
+  plugins: [],
+}
